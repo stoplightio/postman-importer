0e15d1375d3c965414bb9cac40905c632f1da637
agustin <agustin@tekgenesis.com>
2016-12-07 09:46:29 -0300

    DESIGNER-109: Require command line tool arguments

--- bin.js
+++ bin.js
@@ -2,18 +2,19 @@
 const program = require('commander');
 const formats = require('./lib/formats');
 const specConverter = require('./index');
-let file;
-let from;
-let to;
+
+let from = formats.AUTO;
+let to = formats.RAML10;
+let file = undefined;
 
 program
   .arguments('<file>')
-  .option('-f, --from <from>', 'The from/input format, valid values are: swagger, raml08, raml10 and auto (default)')
-  .option('-t, --to <to>', 'The to/target format, valid values are: swagger, raml08 and raml10 (default)')
+  .option('-f, --from <from>', 'The from/input spec, valid values are: swagger, raml08, raml10 and auto (default)')
+  .option('-t, --to <to>', 'The to/target spec, valid values are: swagger, raml08 and raml10 (default)')
   .action(f=> {
     file = f;
-    from = formats[(program.from || 'auto').toUpperCase()];
-    to = formats[(program.to || 'raml10').toUpperCase()];
+    from = !program.from ? from : formats[program.from.toUpperCase()];
+    to = !program.to ? to : formats[program.to.toUpperCase()];
   })
   .parse(process.argv);
 
@@ -22,16 +23,16 @@ const exit = (error) => {
   process.exit(1);
 };
 
-if (typeof to === 'undefined') {
-  exit('Empty or invalid to format given. See --help.');
-}
+if (typeof file === 'undefined') exit('File path required. See --help.');
+if (typeof from === 'undefined') exit('Invalid --from spec given. See --help.');
+if (typeof to === 'undefined') exit('Invalid --to spec given. See --help.');
 
 const converter = new specConverter.Converter(from, to);
-converter.loadFile(file, ()=> {
+converter.loadFile(file, (err)=> {
+  if (err) exit(err);
+
   const format = to.formats[0].toLowerCase();
   converter.convert(format).then(result=>
     console.log(result)
-  ).catch(err=>
-    exit(err)
-  );
+  ).catch(exit);
 });
