0eb5b886a92b011719bb83f466e6b3f4d0abaa5d
Tomas Mateus <tomas@tekgenesis.com>
2017-01-03 17:45:10 -0300

    Bump parser to 1.1.12

--- test/lib/converter.js
+++ test/lib/converter.js
@@ -429,7 +429,6 @@ describe('from raml to swagger', function () {
 			}
 		}
 	});
-	// it('test: ', testWithData("raml10-arrayofarray.yaml", false));
 });
 
 describe.skip('from swagger to raml using apiguru', function () {
