7c6d618cd6ad423cad21a3889ca50326813d169e
Gaston Lodieu <glodieu@gmail.com>
2017-09-01 16:31:49 -0300

    refactor test

--- test/lib/errorLineNumber.js
+++ test/lib/errorLineNumber.js
@@ -1,14 +1,8 @@
 const fs = require('fs');
-const	_ = require('lodash');
 const it = require('mocha/lib/mocha.js').it;
 const describe = require('mocha/lib/mocha.js').describe;
 const RamlErrorLineNumber = require('../../src/helpers/ramlErrorLineNumber');
-
-const chai = require('chai');
-const	expect = chai.expect;
-// const	specConverter = require('../../src/index');
-// const	YAML = require('js-yaml');
-// chai.use(require('chai-string'));
+const expect = require('chai').expect;
 
 describe('Raml10 to line number ', () => {
 	const baseDir = __dirname + '/../data/errorLineNumber/raml';
@@ -17,26 +11,26 @@ describe('Raml10 to line number ', () => {
 	const testWithData = (sourceFile, targetFile) => {
 		return done => {
 			const fileContent = fs.readFileSync(sourceFile, 'utf8');
-			const modelPath = _.trim(fs.readFileSync(sourceFile.replace('.yaml', '.path'), 'utf8'));
+			const modelPath = fs.readFileSync(sourceFile.replace('.yaml', '.path'), 'utf8').trim();
 			const ramlErrorLineNumber = new RamlErrorLineNumber(fileContent, modelPath);
 			const lineNumber = ramlErrorLineNumber.getLineNumber();
-			expect(lineNumber).to.be.equal(_.toInteger(_.trim(fs.readFileSync(targetFile, 'utf8'))));
+			expect(lineNumber).to.be.equal(parseInt(fs.readFileSync(targetFile, 'utf8').trim()));
 
 			return done();
 		};
 	};
 
 	testFiles.forEach(testFile => {
-		if (!_.startsWith(testFile, '.') && _.endsWith(testFile, 'yaml')) {
+		if (!testFile.startsWith('.') && testFile.endsWith('yaml')) {
 			const sourceFile = baseDir + '/' + testFile;
-			const targetFile = baseDir + '/../lineNumber/' + testFile;
+			const targetFile = (baseDir + '/../lineNumber/' + testFile).replace('.yaml', '.txt');
 
 			if (process.env.testFile) {
-				if (_.endsWith(testFile, process.env.testFile)) {
-					it('test: ' + testFile, testWithData(sourceFile, targetFile.replace('.yaml', '.txt')));
+				if (testFile.endsWith(process.env.testFile)) {
+					it('test: ' + testFile, testWithData(sourceFile, targetFile));
 				}
 			} else {
-				it('test: ' + testFile, testWithData(sourceFile, targetFile.replace('.yaml', '.txt')));
+				it('test: ' + testFile, testWithData(sourceFile, targetFile));
 			}
 		}
 	});
