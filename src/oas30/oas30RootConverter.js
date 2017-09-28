99029bc96356a9d369f49ee70c296e2ed07a18e7
Ashley <ulliftw@gmail.com>
2017-09-28 19:21:19 +0200

    :bug: fix issue with baseUri test

--- src/oas30/oas30RootConverter.js
+++ src/oas30/oas30RootConverter.js
@@ -53,6 +53,15 @@ class Oas30RootConverter extends Converter {
 				}
 			}
 
+			if (model.protocols == null && host != null) {
+				const url: string =  host + (basePath || '/');
+				const server = new Server(url);
+				if(!_.isEmpty(variables)) {
+					server.variables = variables;
+				}
+				servers.push(server);
+			}
+
 			if (model.protocols != null && host != null) {
 				const protocols = model.protocols;
 				if (protocol != null && !protocols.includes(protocol)) {
